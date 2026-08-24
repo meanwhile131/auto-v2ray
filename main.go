42df463b041cf242b1c6ca1f66143f9677ab0077
Sasha Chernyakov <sashachernyakov111111@gmail.com>
2026-08-24 22:12:42 +0400

    Switch to libxray

--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"net/http"
 
-	"github.com/meanwhile131/auto-v2ray/parse"
-	"github.com/xtls/xray-core/app/dispatcher"
-	xlog "github.com/xtls/xray-core/app/log"
-	"github.com/xtls/xray-core/app/observatory/burst"
-	"github.com/xtls/xray-core/app/proxyman"
-	"github.com/xtls/xray-core/app/router"
-	clog "github.com/xtls/xray-core/common/log"
+	"github.com/xtls/libxray/share"
 	"github.com/xtls/xray-core/common/net"
-	"github.com/xtls/xray-core/common/serial"
 	"github.com/xtls/xray-core/core"
-	"github.com/xtls/xray-core/proxy/freedom"
-	"github.com/xtls/xray-core/proxy/socks"
+	"github.com/xtls/xray-core/infra/conf"
+	_ "github.com/xtls/xray-core/main/distro/all"
 )
 
 func main() {
@@ -30,61 +24,38 @@ func main() {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	outbounds, err := parse.URLFile(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	config, err := share.ConvertShareLinksToXrayJson(string(body))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	inbound := core.InboundHandlerConfig{
-		Tag: "in",
-		ReceiverSettings: serial.ToTypedMessage(&proxyman.ReceiverConfig{
-			PortList: &net.PortList{Range: []*net.PortRange{net.SinglePortRange(9000)}},
-			Listen:   net.NewIPOrDomain(net.ParseAddress("127.0.0.1")),
-		}),
-		ProxySettings: serial.ToTypedMessage(&socks.ServerConfig{}),
+	for i := range config.OutboundConfigs {
+		config.OutboundConfigs[i].Tag = fmt.Sprintf("out%d", i)
+		config.OutboundConfigs[i].SendThrough = nil
 	}
-	freedom := core.OutboundHandlerConfig{
-		Tag:            "freedom",
-		SenderSettings: serial.ToTypedMessage(&proxyman.SenderConfig{}),
-		ProxySettings:  serial.ToTypedMessage(&freedom.Config{}),
+	config.OutboundConfigs = append(config.OutboundConfigs, conf.OutboundDetourConfig{
+		Tag:      "freedom",
+		Protocol: "freedom",
+	})
+	config.InboundConfigs = append(config.InboundConfigs, conf.InboundDetourConfig{
+		PortList: &conf.PortList{Range: []conf.PortRange{{
+			From: 9000,
+			To:   9000,
+		}}},
+		Protocol: "socks",
+		Tag:      "in",
+		ListenOn: &conf.Address{Address: net.ParseAddress("127.0.0.1")}},
+	)
+
+	cfg, err := config.Build()
+	if err != nil {
+		log.Fatal(err)
 	}
-	outbounds = append(outbounds, &freedom)
-	config := core.Config{
-		App: []*serial.TypedMessage{
-			serial.ToTypedMessage(&xlog.Config{
-				ErrorLogLevel: clog.Severity_Info,
-				ErrorLogType:  xlog.LogType_Console,
-				AccessLogType: xlog.LogType_Console,
-			}),
-			serial.ToTypedMessage(&dispatcher.Config{}),
-			serial.ToTypedMessage(&proxyman.InboundConfig{}),
-			serial.ToTypedMessage(&proxyman.OutboundConfig{}),
-			serial.ToTypedMessage(&router.Config{
-				BalancingRule: []*router.BalancingRule{{
-					Tag:              "router",
-					OutboundSelector: []string{"out"},
-					Strategy:         "leastload",
-					StrategySettings: serial.ToTypedMessage(&router.StrategyLeastLoadConfig{}),
-					FallbackTag:      "freedom",
-				}},
-				Rule: []*router.RoutingRule{{
-					InboundTag: []string{"in"},
-					TargetTag: &router.RoutingRule_BalancingTag{
-						BalancingTag: "router",
-					},
-				}},
-			}),
-			serial.ToTypedMessage(&burst.Config{
-				SubjectSelector: []string{"out"},
-				PingConfig: &burst.HealthPingConfig{
-					Interval: int64(10 * time.Minute),
-				},
-			}),
-		},
-		Inbound:  []*core.InboundHandlerConfig{&inbound},
-		Outbound: outbounds,
-	}
-	in, err := core.New(&config)
+	in, err := core.New(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -93,12 +64,13 @@ func main() {
 		log.Fatal(err)
 	}
 	defer in.Close()
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	<-c
+	log.Println("Exiting")
 	err = in.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Exiting")
 }
